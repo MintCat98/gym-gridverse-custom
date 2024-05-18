ff7f77174ce6379aae917648946c695e38a4fa14
Hojeong Kim <cabi930@gmail.com>
2024-05-18 15:06:18 +0900

    remove gym version specification; prevent further package error

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [
     'pyglet<=1.5.27',
-    'gym<=0.21.0',
+    # 'gym<=0.21.0',
+    'gym',
     'imageio',
     'imageio-ffmpeg',
     'more_itertools',
